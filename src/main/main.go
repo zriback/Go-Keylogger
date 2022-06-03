08e46bf495f24b2ae3ca091b1bdcc9431c43f83d
zriback <zacharythecdr@gmail.com>
2022-06-03 01:26:04 -0400

    Added map containing keycodes

--- src/main/main.go
+++ src/main/main.go
@@ -8,8 +8,8 @@ import (
 )
 
 var (
-	user32             = syscall.NewLazyDLL("user32.dll")
-	processessGetState = user32.NewProc("GetAsyncKeyState")
+	user32   = syscall.NewLazyDLL("user32.dll")
+	getState = user32.NewProc("GetAsyncKeyState")
 )
 
 func main() {
@@ -18,12 +18,25 @@ func main() {
 	// wait for a bit
 	time.Sleep(5000 * time.Millisecond)
 
+	keysDown := 0
+
 	for key := 0; key <= 256; key++ {
 
 		// get value, don't care about the other two values here
-		value, _, _ := processessGetState.Call(uintptr(key))
-		fmt.Print(value)
+		value, _, _ := getState.Call(uintptr(key))
+		//fmt.Print(value)
+
+		fmt.Print("Key #: ", key)
+
+		if value == 32769 {
+			fmt.Println("\tYES")
+			keysDown++
+		} else {
+			fmt.Println("\tNO")
+		}
 	}
+
+	fmt.Println("\nKeys presses: ", keysDown)
 
 }
 
